93e78b838dc392f90cb71bffd17970c791e5c6ab
kranthi8563 <112112567+kranthi8563@users.noreply.github.com>
2023-01-09 20:20:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,28 +2,46 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+//set EJS as view engine
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodos();
+  // rendering the content
+  if (request.accepts("html")) {
+    response.render("index", {
+      allTodos,
+    });
+  } else {
+    response.json({
+      allTodos,
+    });
+  }
 });
 
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(`${__dirname}/public/`)));
+
+app.get("/", function (request, response) {
+  response.send("Todo list", request.body);
+});
+
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
   // FILL IN YOUR CODE HERE
-
-  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
-  // Then, we have to respond with all Todos, like:
-  // response.send(todos)
-
   try {
-    const todos = await Todo.findAll({ order: [["id", "ASC"]] });
-    return response.json(todos);
+    const todos = await Todo.findAll();
+    return response.send(todos);
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
+  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
+  // Then, we have to respond with all Todos, like:
+  // response.send(todos)
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -59,13 +77,16 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
-
   // FILL IN YOUR CODE HERE
-
+  const deleteTodo = await Todo.destroy({
+    where: {
+      id: request.params.id,
+    },
+  });
+  response.send(deleteTodo ? true : false);
   // First, we have to query our database to delete a Todo by ID.
-  const affectedRow = await Todo.destroy({ where: { id: request.params.id } });
   // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  response.send(affectedRow ? true : false);
+  // response.send(true)
 });
 
 module.exports = app;
