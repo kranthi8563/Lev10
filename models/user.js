76f60347004e13714f768be0ccde3a11425ea546
kranthi8563 <112112567+kranthi8563@users.noreply.github.com>
2023-01-15 16:19:05 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -11,14 +11,39 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(models.Todo, {
         foreignKey: "userId",
       });
-      // define association here
+    }
+
+    static createUser({ firstName, lastName, email, password }) {
+      return this.create({ firstName, lastName, email, password });
     }
   }
   User.init(
     {
-      firstName: DataTypes.STRING,
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "First name is required",
+          },
+          notEmpty: {
+            msg: "First name is required",
+          },
+        },
+      },
       lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Email is required",
+          },
+          notEmpty: {
+            msg: "Email is required",
+          },
+        },
+      },
       password: DataTypes.STRING,
     },
     {
